644aaa1c542562cfd98f3b7cf58c081c88cddaf8
Darkø Tasevski <darko.tasevski@redbox.com>
2020-03-08 20:57:05 +0100

    build: fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@
 set -e
 
 # build
-npm run docs:build
+npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:Puritanic/CompSci.git master:gh-pages
 
 cd -
